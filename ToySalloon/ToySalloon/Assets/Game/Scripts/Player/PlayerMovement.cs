f67fcaa22cb7f506270d7104d4f375aa05fd8f61
LucasHome <lucashilgevoord@hotmail.com>
2020-04-09 12:31:05 +0200

    Created basic player movement

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private void OnEnable()
+    {
+        InputManager.OnObjectClicked += OnFloorPressed;
+    }
+    private void OnDisable()
+    {
+        InputManager.OnObjectClicked -= OnFloorPressed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnFloorPressed(Transform t)
+    {
+        if (t.gameObject.tag == "Ground")
+        {
+            Vector3 newPos;
+            Ray ray = Application.isEditor ? Camera.main.ScreenPointToRay(Input.mousePosition) : Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPos = hit.point;
+                MovePlayerToPos(newPos);
+            }
+        }
+    }
+
+    public void MovePlayerToPos(Vector3 newPos)
+    {
+        agent.SetDestination(newPos);
+    }
+}
