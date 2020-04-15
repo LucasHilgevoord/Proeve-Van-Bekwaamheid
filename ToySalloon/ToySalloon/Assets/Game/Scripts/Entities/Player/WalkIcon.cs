86672687b21ae024bd82ab8ca201057678f8ba72
LucasHome <lucashilgevoord@hotmail.com>
2020-04-15 11:32:28 +0200

    Moved player scripts

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/Player/WalkIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkIcon : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    private float distance = 1.3f;
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.position) < distance)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
