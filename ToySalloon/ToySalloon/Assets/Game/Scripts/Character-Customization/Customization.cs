163319c3c48a413cd6ded9fc09ffb2afa937747a
MaxHDekker <xamrekked@gmail.com>
2020-04-16 11:46:22 +0200

    Implemented Customizables

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Character-Customization/Customization.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Customizable", menuName = "Customizable")]
+public class Customization : ScriptableObject
+{
+    public Sprite customizable;
+    public Color color;
+}
