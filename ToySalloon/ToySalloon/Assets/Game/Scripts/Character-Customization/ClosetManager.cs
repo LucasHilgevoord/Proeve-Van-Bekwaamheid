e29a8ddbd4000e0d61a59d5d408aaa559ca2c615
MaxHDekker <xamrekked@gmail.com>
2020-04-16 11:45:30 +0200

    Implemented Customization Closet.

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Character-Customization/ClosetManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClosetManager : MonoBehaviour
+{
+    public SpriteRenderer characterPart;
+
+    [SerializeField]
+    private List<GameObject> drawers = new List<GameObject>();
+
+    private GameObject currentDrawer;
+
+    private void Start()
+    {
+        currentDrawer = drawers[0];
+
+        CheckActive();
+    }
+
+    public void SwitchDrawer(GameObject drawer)
+    {
+        currentDrawer = drawer;
+        CheckActive();
+    }
+
+    private void CheckActive()
+    {
+        foreach (GameObject drawer in drawers)
+        {
+            if (drawer == currentDrawer)
+            {
+                drawer.gameObject.SetActive(true);
+                characterPart = drawer.GetComponent<ClosetPlayerLink>().part;
+            }
+            else
+            {
+                drawer.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Character-Customization/ClosetPlayerLink.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosetPlayerLink : MonoBehaviour
+{
+    public SpriteRenderer part;
+}
