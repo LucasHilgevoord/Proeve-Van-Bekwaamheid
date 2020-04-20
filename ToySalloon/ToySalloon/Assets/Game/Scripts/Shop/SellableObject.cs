d7ffca3fd548c1d3e6877193c269e97b467881ec
LucasHome <lucashilgevoord@hotmail.com>
2020-04-20 19:33:27 +0200

    Added basic furniture interaction

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Shop/SellableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SellableObject : MonoBehaviour
+{
+    private enum objectType // All types that can be chosen for the sellable objects
+    {
+        POTION,
+        ARMOR,
+        WEAPONS,
+        PLUSHY,
+        MAGIC
+    }
+
+    [SerializeField]
+    private objectType type; // The type of the sellable object.
+
+    public Image itemImage; // The image displayed in the buy window.
+    public string itemName; // The name displayed in the buy window.
+    public float price; // The price of the object.
+}
--- ToySalloon/ToySalloon/Assets/Game/Scripts/Shop/StandingPoint.cs
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Shop/StandingPoint.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 [Serializable]
 public class StandingPoint
 {
-    public Transform standingPoints; // Positions of the predefined places where npc's can stand.
+    public Transform standingPoint; // Positions of the predefined places where npc's can stand.
     public bool isOccupied; // Is the standing place occupied by a NPC?
 }
