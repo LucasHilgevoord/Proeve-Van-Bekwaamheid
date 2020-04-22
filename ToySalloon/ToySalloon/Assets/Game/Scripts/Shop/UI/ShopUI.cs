f19e08ac8213e44387a023b768add478f2961543
LucasHome <lucashilgevoord@hotmail.com>
2020-04-22 15:41:26 +0200

    Added simple StoreUI Animation

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Shop/UI/ShopUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUI : MonoBehaviour
+{
+    private Animation anim;
+
+    private void OnEnable()
+    {
+        UIWindow.OnWindowOverlay += DisableUI;
+    }
+    private void OnDisable()
+    {
+        UIWindow.OnWindowOverlay -= DisableUI;
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    private void DisableUI(bool isOpen)
+    {
+        string name = isOpen ? "StoreUIOff" : "StoreUIOn";
+        anim.Play(name);
+    }
+}
