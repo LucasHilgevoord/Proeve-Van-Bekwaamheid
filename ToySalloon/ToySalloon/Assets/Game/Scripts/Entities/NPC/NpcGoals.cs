e56f4dc4b5e6448c8b486855ff563f502683816d
LucasHome <lucashilgevoord@hotmail.com>
2020-04-22 15:51:32 +0200

    Added simple decide for npc purpose

--- ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/NPC/NpcGoals.cs
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/NPC/NpcGoals.cs
@@ -4,7 +4,7 @@
 public enum NpcGoals
 {
     BUY, // Wanting to buy an item.
-    SELL, // Wanting to sell an item.
     REPAIR, // Wanting to get something repaired.
-    LOOKAROUND // Only look around without buying/selling/repairing.
+    LOOKAROUND, // Only look around without buying/selling/repairing.
+    SELL // Wanting to sell an item.
 }
