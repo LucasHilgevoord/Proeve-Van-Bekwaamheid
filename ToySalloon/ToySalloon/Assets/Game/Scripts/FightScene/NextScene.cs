9bb9c6d2a97e31254e88424a6f3efb4e1b6c1b2f
MaxHDekker <xamrekked@gmail.com>
2020-04-22 16:35:18 +0200

    Temp updates for demo

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/FightScene/NextScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextScene : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            //doe iets lucas, help me
+        }
+    }
+}
