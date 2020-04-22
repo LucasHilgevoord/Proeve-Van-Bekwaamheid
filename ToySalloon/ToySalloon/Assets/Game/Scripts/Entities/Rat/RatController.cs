da74249259e4126ea17867aa90cf75b94372056b
LucasHome <lucashilgevoord@hotmail.com>
2020-04-22 15:50:41 +0200

    Added simple rat controller

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/Rat/RatController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    private bool isCaught;
+    private float distance = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isCaught && Vector3.Distance(transform.position, player.position) < distance)
+        {
+            isCaught = true;
+            WorldManager.SharedInstance.FadeToScene(0);
+        }
+    }
+}
