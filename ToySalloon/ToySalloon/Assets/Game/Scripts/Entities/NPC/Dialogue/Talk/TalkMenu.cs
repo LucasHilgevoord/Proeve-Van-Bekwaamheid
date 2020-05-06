436970e169a840440cd66adc0d687df16185a26a
LucasHome <lucashilgevoord@hotmail.com>
2020-05-06 14:55:18 +0200

    Added animation for small talk dialogue

--- ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/NPC/Dialogue/Talk/TalkMenu.cs
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/NPC/Dialogue/Talk/TalkMenu.cs
@@ -6,12 +6,28 @@ public class TalkMenu : UIWindow
 {
     [SerializeField]
     private NpcController npc;
+    [SerializeField]
+    private Animator animator;
+
+    private void Update()
+    {
+        if (animator.GetBool("shouldClose") && AnimatorIsPlaying())
+        {
+            npc.currentState = npc.prevState;
+            npc.agent.destination = npc.destination.position;
+            animator.SetBool("shouldClose", false);
+            Debug.Log(npc.currentState.ToString());
+            gameObject.SetActive(false);
+        }
+    }
 
     public void DisableWindow()
     {
-        npc.currentState = npc.prevState;
-        npc.agent.destination = npc.destination.position;
-        //Animation
-        gameObject.SetActive(false);
+        animator.SetBool("shouldClose", true);
+    }
+
+    public bool AnimatorIsPlaying()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).normalizedTime % 1 > 0.99f;
     }
 }
--- ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/NPC/States/NpcGoTalk.cs
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/NPC/States/NpcGoTalk.cs
@@ -16,6 +16,7 @@ public class NpcGoTalk : State
     private void Talk()
     {
         c.prevState = c.currentState;
+        Debug.Log(c.prevState.ToString());
         c.agent.destination = transform.position;
         c.currentState = NpcStates.TALK;
         c.conversationObj.SetActive(true);
