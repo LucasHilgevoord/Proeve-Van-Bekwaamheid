d8292595aff07fd8211ca3a848b3588df9af5f3d
MaxHDekker <xamrekked@gmail.com>
2020-05-12 11:28:13 +0200

    Claw damage implemented

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/FightScene/ClawDamage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawDamage : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("hit");
+        }
+    }
+}
