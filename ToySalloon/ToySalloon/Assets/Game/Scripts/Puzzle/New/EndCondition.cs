06cbf7af358e3646aa3eaf9fab97f43385f6d574
MaxHDekker <xamrekked@gmail.com>
2020-05-16 01:11:05 +0200

    Win lose condition created + implemented, puzzle scene

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Puzzle/New/EndCondition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCondition : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject winScreen;
+
+    public GameObject loseScreen;
+
+    public void Win()
+    {
+        winScreen.SetActive(true);
+    }
+    
+    public void Lose()
+    {
+        loseScreen.SetActive(true);
+    }
+}
