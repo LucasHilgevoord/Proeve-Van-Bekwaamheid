17fa5c5f9eaac613e2630af62a6e6f38a01b665b
Troy2000 <troy_toendang@hotmail.com>
2020-05-18 12:17:01 +0200

    Implemented Rat indicator animation

--- ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/Rat/RatController.cs
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Entities/Rat/RatController.cs
@@ -6,11 +6,25 @@ public class RatController : MonoBehaviour
 {
     [SerializeField]
     private Transform player;
+
     private bool isCaught;
     private float distance = 1.5f;
 
+    private Animator ratAnim;
+
+    private void Start()
+    {
+        ratAnim = transform.GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
+    {
+        ActivateFightScene();
+    }
+
+    // Activate the fight scene
+    void ActivateFightScene()
     {
         if (!isCaught && Vector3.Distance(transform.position, player.position) < distance)
         {
---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Managers/RatManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatManager : MonoBehaviour
+{
+    // Where the rat spawns
+    [SerializeField]
+    private GameObject ratSpawnPoint;
+
+    // The rat prefab
+    public GameObject ratPrefab;
+
+    public List<GameObject> placedPieces = new List<GameObject>();
+    public List<int> puzzleOrder = new List<int>();
+
+    private Vector3 startPos;
+    //private float offset = 0;
+    //public float placeHolderCount = 0;
+
+    void Start()
+    {
+        // Where the first placeholder should be put
+        startPos = ratSpawnPoint.GetComponent<RectTransform>().anchoredPosition;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SpawnRat();
+        }
+    }
+
+    // Put placeholders on board
+    public void SpawnRat()
+    {
+        //offset += 160;
+        CreateRat(new Vector3(startPos.x, startPos.y, startPos.z));
+    }
+
+    // Create the placeholder
+    void CreateRat(Vector3 spawnPosition)
+    {
+        //placeHolderCount += 1;
+        Instantiate(ratPrefab, spawnPosition, Quaternion.identity);
+    }
+}
