dabe9da9e815a3fcb6f514c3aa39d8e0fa7acb45
MaxHDekker <xamrekked@gmail.com>
2020-05-18 16:15:54 +0200

    Tutorial logic created

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Tutorial/GUITutorial.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;   
+
+public class GUITutorial : MonoBehaviour
+{
+    [SerializeField]
+    private Text title;
+    [SerializeField]
+    private TextMeshProUGUI textbox;
+
+    private string currentText;
+    private bool active = true;
+
+    public void LinkUI(string title, string textbox)
+    {
+        if(this.title.text == "")
+        {
+            this.title.text = title;
+        }
+
+        if (textbox != "")
+        {
+            StartCoroutine(ShowMessage(textbox));
+        }
+    }
+
+    private IEnumerator ShowMessage(string mes)
+    {
+        for (int i = 0; i < mes.Length; i++)
+        {
+            currentText = mes.Substring(0, i);
+            textbox.text = currentText;
+            yield return new WaitForSeconds(0.03f);
+
+            if (!active)
+            {
+                yield break;
+            }
+        }
+    }
+}
---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Tutorial/HandleTutorial.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleTutorial : MonoBehaviour
+{
+    [SerializeField]
+    private Tutorial sceneTutorial;
+
+    private GUITutorial tutorialInterface;
+
+    private int textIndex = 0;
+
+    private void Start()
+    {
+        tutorialInterface = GetComponent<GUITutorial>();
+        LinkInterface(textIndex);
+    }
+
+    private void LinkInterface(int index)
+    {
+        tutorialInterface.LinkUI(sceneTutorial.title, sceneTutorial.bigMessage[index]);
+        textIndex++;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ManageTutorial();
+        }
+
+    }
+
+    private void ManageTutorial()
+    {
+        if (textIndex < sceneTutorial.bigMessage.Length)
+        {
+            LinkInterface(textIndex);
+        }
+        else
+        {
+            Debug.Log("nope :)");
+        }
+    }
+}
