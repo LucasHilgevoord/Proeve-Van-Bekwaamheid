be8aa5508138d4bf63f560a7d1fe1d949847f702
MaxHDekker <xamrekked@gmail.com>
2020-05-25 23:22:12 +0200

    Start screen manager created

---
+++ ToySalloon/ToySalloon/Assets/Game/Scripts/Main Screen/MainscreenManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainscreenManager : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas selectCanvas;
+    [SerializeField]
+    private Animator logoCanvas;
+    [SerializeField]
+    private Animator logo;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeState();
+        }
+    }
+
+    private void ChangeState()
+    {
+        selectCanvas.gameObject.SetActive(true);
+        Camera.main.GetComponent<Animator>().SetTrigger("GotoSelect");
+        logo.SetTrigger("LogoOutro");
+        logoCanvas.SetTrigger("KlikOpOutro");
+    }
+}
